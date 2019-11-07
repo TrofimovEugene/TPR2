71801ac75895c0da3dc635f6e2714d82bf7327fe
Trofimov Eugene <evgentrofi@gmail.com>
2019-11-07 18:25:50 +0800

    Обновление целевой платформы проекта

---
+++ NormalRandom.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TPR2
+{
+    // класс для задания случайных чисел по нормальному закону
+    class NormalRandom: Random
+    {
+        double prevSample = double.NaN;
+        protected override double Sample()
+        {
+            if (!double.IsNaN(prevSample))
+            {
+                double result = prevSample;
+                prevSample = double.NaN;
+                return result;
+            }
+
+            double u, v, s;
+            do
+            {
+                u = 2 * base.Sample() - 1;
+                v = 2 * base.Sample() - 1;
+                s = u * u + v * v;
+            } while (u <= -1 || v <= -1 || s >= 1 || s == 0);
+            double r = Math.Sqrt(-2 * Math.Log(s) / s);
+            prevSample = r * v;
+            return r * u;
+        }
+    }
+}
---
+++ Point.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPR2
+{
+    // класс, описывающий случаную точку
+    class Point
+    {
+        private double x;
+        private double y;
+		private double z;
+        // мат. ожидание по X
+        public double mu_x;
+        // мат. ожидание по Y
+        public double mu_y;
+        // среднеквадратическое отклонение X
+        public double sigma_x { get; set; }
+        // среднеквадратическое отклонение Y
+        public double sigma_y { get; set; }
+		NormalRandom nr;
+        public Point(double m_x, double s_x, double m_y, double s_y, NormalRandom norm_rand)
+        {
+            mu_x = m_x;
+            sigma_x = s_x;
+            mu_y = m_y;
+            sigma_y = s_y;
+            nr = norm_rand;
+            Set_Value();
+        }
+		public Point(double x, double y, double z)
+		{
+			this.x = x;
+			this.y = y;
+			this.z = z;
+		}
+        // инициализация значений
+        void Set_Value()
+        {
+            x = nr.NextDouble() * sigma_x + mu_x;
+            y = nr.NextDouble() * sigma_y + mu_y;
+        }
+		public void Set_Value_z(double z)
+		{
+			this.z = z;
+		}
+        // возврат значения X
+        public double Return_Value_x()
+        {
+            return x;
+        }
+        // возврат значения Y
+        public double Return_Value_y()
+        {
+            return y;
+        }
+		public double Return_Value_z()
+		{
+			return z;
+		}
+    }
+}
